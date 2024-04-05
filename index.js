aad5350fc6ad1baba2eecfb0f20e8439808a8264
sam-fsd <kabirusam037@gmail.com>
2024-04-05 16:59:09 +0300

    notes app

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+let notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    important: true,
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only JavaScript',
+    important: false,
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    important: true,
+  },
+];
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+
+  return maxId + 1;
+};
+
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use(
+  morgan(':method :url status :res[content-length] - :response-time ms :body')
+);
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World</h1>');
+});
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.statusMessage = 'The note your requested was not found :(';
+    response.status(404).end();
+  }
+});
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing',
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
