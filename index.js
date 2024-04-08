f70e6d9919bb73638486d3bed0b9d2c69374b688
sam-fsd <kabirusam037@gmail.com>
2024-04-08 09:09:39 +0300

    serving static files

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ morgan.token('body', (req, res) => JSON.stringify(req.body));
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static('dist'));
 
 app.use(
   morgan(':method :url status :res[content-length] - :response-time ms :body')
